f52f13e5975128789c918cf637326d7b4cc3c2eb
Lukas A. Santiago <lukas.asant@gmail.com>
2021-09-10 19:44:27 -0300

    delete old files

--- src/OLD/Model/EnumColor.java
+++
@@ -1,5 +0,0 @@
-package OLD.Model;
-
-public enum EnumColor {
-    YELLOW, RED, GREEN, BLUE, GRAY, CYAN, BLACK, MAGENTA;
-}
--- src/OLD/Model/EnumSize.java
+++
@@ -1,5 +0,0 @@
-package OLD.Model;
-
-public enum EnumSize {
-    P, M, G
-}
